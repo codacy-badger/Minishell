b4cd63c5922aa0ab7c3ff99111791041ff8f91bd
Ant0wan <abarthel@student.42.fr>
2019-06-26 22:00:07 +0200

    go home forks are working and exit command too

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 18:32:13 by abarthel          #+#    #+#             */
-/*   Updated: 2019/06/20 19:48:42 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/06/26 21:58:40 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,26 @@ int	main(int argc, char **argv, char **env)
 	(void)env;
 	int stat;
 	int i;
-	char buf[10000];
+	char buf[100];
 
-	ft_bzero((void*)buf, 10000);
-	while (ft_printf("%s@home ~$ ", getenv("USER")) && read(1, &buf, 10000))
+	ft_bzero((void*)buf, 100);
+	while (ft_printf("%s@home ~$ ", getenv("USER")) && read(1, &buf, 100))
 	{
+		i = 0;
+		while (buf[i] != '\n')
+			++i;
+		if (buf[i] == '\n')
+			buf[i] = '\0';
+		if (!ft_strcmp(buf, "exit"))
+			return (0);
 		stat = 0;
 		if (fork() == 0)
 		{
-			i = 0;
-			while (buf[i] != '\n')
-				++i;
-			if (buf[i] == '\n')
-				buf[i] = '\0';
-			execve(buf, argv, env);
+			return (execve(buf, argv, env));
 		}
 		else
-		{
 			wait(&stat);
-			ft_bzero((void*)buf, 10000);
-		}
+		ft_bzero((void*)buf, 100);
 	}
 	return (0);
 }
