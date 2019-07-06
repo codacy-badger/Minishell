609501cc903f82cf35a21627cf4e3873bb423b74
Ant0wan <abarthel@student.42.fr>
2019-07-06 21:59:12 +0200

    go

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 18:32:13 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/06 21:52:29 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/07/06 21:59:01 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 int	main(void)
 {
-	extern char	**environ;
+	extern char			**environ;
 	int			stat;
 	int			ret_fork;
 	char		*buf;
@@ -38,9 +38,9 @@ int	main(void)
 		{
 			if (access(buf, 1000))
 			{
+				ft_dprintf(STDERR_FILENO, "%s: command not found: %s\n", argv[0], buf);
 				ft_tabdel(&environ);
 				ft_memdel((void**)&buf);
-				ft_dprintf(STDERR_FILENO, "%s: command not found: %s\n", argv[0], buf);
 				exit (127);
 			}
 			ret_fork = execve(buf, argv, environ);
