8c5c2172d1c11716cc79cfc40e01b1e994c538f7
Ant0wan <abarthel@student.42.fr>
2019-07-25 21:48:33 +0200

    test access and type

--- src/job/job.c
+++ src/job/job.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 13:03:13 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/25 21:00:11 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/07/25 21:48:17 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include "builtins.h"
 #include "error.h"
 #include "libft.h"
 
 static int	check_access(char *arg)
 {
-	if (access(arg, F_OK))
+	int ret;
+	struct stat buf;
+
+	if (lstat(arg, &buf) != -1)
 	{
-		return (e_command_not_found);
+		if (buf)
+		/* code generic function that return the type of the arg using lstat or fstat or stat*/
+	}
+	else if ((ret = access(arg, F_OK)))
+	{
+		return (ret);
 	/* concat the path and command name to PATH and test it */
 	}
-	else if (access(arg, X_OK))
+	else if ((ret = access(arg, X_OK)))
 	{
 		psherror(e_permission_denied, arg);
-		return (e_permission_denied);
+		return (ret);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
 
 static int	builtin_keyword_exec(char **argv)
@@ -86,8 +92,11 @@ int	job(char **argv, char **envp)
 	else if (!(ret = check_access(argv[0])))
 	{
 		ret = process_launch(argv, envp);
-		return (g_errordesc[ret].code);
-	}
+		ft_printf("ret:%d\n", ret);
+		perror(NULL);
+		return (ret);
+		/*return (g_errordesc[ret].code);
+*/	}
 	else
 	{
 		ret = builtins_select(&argv[i]);
