3837dbb18422c95c762373e88093998ec3a9426b
Ant0wan <abarthel@student.42.fr>
2019-08-01 10:17:34 +0200

    test script added

---
+++ tools/tests/launch_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    launch_test.sh                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/08/01 10:05:56 by abarthel          #+#    #+#              #
+#    Updated: 2019/08/01 10:05:56 by abarthel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+MINISHELL_PATH='../../'
+tests_array=(*.test)
+
+make -j -C $MINISHELL_PATH
+
+for file in "${tests_array[@]}"
+do
+	$MINISHELL_PATH./minishell < $file
+done
+
+make fclean -C $MINISHELL_PATH
