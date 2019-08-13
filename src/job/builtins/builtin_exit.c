eee1f004d6d50c0683a59232c2afc5922904f741
Ant0wan <abarthel@student.42.fr>
2019-08-13 08:28:34 -0400

    exit builtin ok

--- src/job/builtins/builtin_exit.c
+++ src/job/builtins/builtin_exit.c
@@ -12,13 +12,29 @@
 
 #include <unistd.h>
 #include "libft.h"
+#include "error.h"
 
 int	cmd_exit(int argc, char **argv)
 {
-	extern char **environ;
+	extern char	**environ;
+	unsigned char	status;
 
-	(void)argc;
+	status = 0;
+	if (argc == 2)
+	{
+		if (*argv[1]
+			&& (((*argv[1] == '-' || *argv[1] == '+') && ft_str_is_numeric(&argv[1][1]))
+			|| ft_str_is_numeric(argv[1])))
+				status = (unsigned char)ft_atoi(argv[1]);
+		else
+		{
+			ft_dprintf(STDERR_FILENO,
+			"%s: %s: %s: numeric argument required\n",
+					g_progname, argv[0], argv[1]);
+			return (2);
+		}
+	}
 	ft_tabdel(&argv);
 	ft_tabdel(&environ);
-	exit(0);
+	exit(status);
 }
